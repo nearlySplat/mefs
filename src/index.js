68665a2ab6831c31399cb7076b35943344c654e8
nearlySplat <splatterxl@outlook.ie>
2021-01-18 17:09:50 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ class Mefs {
   static E = "E"
   static pow (power, ...nopower) {
     if (!power || !nopower) throw new Error(`Expected two or more arguments, got ${power ? "one" : "none"}.`);
-    return `${power} HAS POWERR` + nopower ? `,\n\n${nopower.map((v, i, a) => i == (a.length - 1) ? v : i == (a.length - 2) ? `${v} and ` : `${v}, `).join("")} do not :(` : "";
+    return `${power} HAS POWERR,\n\n${nopower.map((v, i, a) => i == (a.length - 1) ? v : i == (a.length - 2) ? `${v} and ` : `${v}, `).join("")} do${nopower.length == 1 ? "es" : ""} not :(` : "";
   }
 }
 

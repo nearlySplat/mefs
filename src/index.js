66cfd34479f90a38f27fab837a911a2dcc4afaa3
nearlySplat <splatterxl@outlook.ie>
2021-01-18 16:47:09 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const Constants = require("./static/constants")
 class Mefs {
   constructor () {
-    
+    throw new Error(`The ${this.constructor.name} class may not be instantiated.`)
   }
-  random() {
+  static random() {
     return Constants.random[Math.floor(Math.random() * Constants.random.length)]
   }
+  static PIE = "🥧"
+  static E = "E"
 }
 
 module.exports = Mefs

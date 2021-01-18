6bee974c820a91173e6cce068a37d275e0567a37
nearlySplat <splatterxl@outlook.ie>
2021-01-18 17:11:19 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ class Mefs {
   }
   static PIE = "🥧"
   static E = "E"
-  static pow (power, ...nopower) {
-    if (!power || !nopower) throw new Error(`Expected two or more arguments, got ${power ? "one" : "none"}.`);
-    return `${power} HAS POWERR,\n\n${nopower.map((v, i, a) => i == (a.length - 1) ? v : i == (a.length - 2) ? `${v} and ` : `${v}, `).join("")} do${nopower.length == 1 ? "es" : ""} not :(`;
+  static pow (...data) {
+    if (!data[1]) throw new Error(`Expected two or more arguments, got ${data[0] ? "one" : "none"}.`);
+    return `${data[0]} HAS POWERR,\n\n${data.slice(1).map((v, i, a) => i == (a.length - 1) ? v : i == (a.length - 2) ? `${v} and ` : `${v}, `).join("")} do${nopower.length == 1 ? "es" : ""} not :(`;
   }
 }
 

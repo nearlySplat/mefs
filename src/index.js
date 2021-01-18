ae6a0d5ad49d2917cdf5e4bd1a383a4b041b3092
nearlySplat <splatterxl@outlook.ie>
2021-01-18 14:14:32 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const Constants = require("static/contants")
+
+exports = class Mefs extends Math {
+  constructor () {
+    throw new Error(`The ${this.constructor.name} class may not be instantiated!`);
+  }
+  static random () {
+    return Constants.random[super.floor(super.random() * Constants.random.length)]
+  }
+}

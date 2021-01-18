87cf23ddfdb06c1582de7a244d36f803b6ac0c85
nearlySplat <splatterxl@outlook.ie>
2021-01-18 20:24:31 +0000

    change "does" and "has" to "do" and "have" if the data includes a known pronoun.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ class Mefs {
   static E = "E"
   static pow (...data) {
     if (!data[1]) throw new Error(`Expected two or more arguments, got ${data[0] ? "one" : "none"}.`);
-    return `${data[0]} HAS POWERR,\n\n${data.slice(1).map((v, i, a) => i == (a.length - 1) ? v : i == (a.length - 2) ? `${v} and ` : `${v}, `).join("")} do${data.slice(1).length == 1 ? "es" : ""} not :(`;
+    data = data.map(v => typeof v == "number" ? v.toString() : v)
+    return `${data[0]} HA${["i", "you", "they"].includes(data[0].toLowerCase()) ?  "VE" : "S"} POWERR,\n\n${data.slice(1).map((v, i, a) => i == (a.length - 1) ? v : i == (a.length - 2) ? `${v} and ` : `${v}, `).join("")} do${data.slice(1).length == 1 && data.every(v => !["i", "you", "they"].includes(v.toLowerCase())) ? "es" : ""} not :(`;
   }
 }
 
